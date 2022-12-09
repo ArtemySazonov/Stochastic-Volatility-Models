10bfba4aa4ea568da63d3ee359f96a9aa0361fd7
Artemy <artemy.sazonov@gmail.com>
2022-12-09 20:57:11 +0300

    added controlled_simulation

--- Presentations/Code/batch.py
+++ Presentations/Code/batch.py
@@ -1,5 +1,16 @@
 import scipy.stats
 import numpy as np
+from dataclasses import dataclass
+from typing import Union, Callable, Optional
+
+@dataclass
+class HestonParameters:
+    kappa:  Union[float, np.ndarray]
+    gamma:  Union[float, np.ndarray]
+    rho:    Union[float, np.ndarray]
+    vbar:   Union[float, np.ndarray]
+    v0:     Union[float, np.ndarray]
+
 
 def get_len_conf_interval(data: np.ndarray, 
                           confidence_level: float = 0.05):
@@ -35,10 +46,31 @@ def get_number_of_batches(data: np.ndarray,
                           batch_size: int = 10000):
     """Get the number of batches needed to achieve a given relative error using the MC simulations.
     Args:
-        absol_error: The relative error to achieve. Defaults to 0.005 (cor˝responds to 0.5 cents).
+        absol_error: The relative error to achieve. Defaults to 0.005 (corresponds to 0.5 cents).
         batch_size: The batch size to use. Defaults to 10000.
     
     Returns:
         The number of batches needed to achieve the given relative error.
     """
     return int(get_number_of_simulations(data, confidence_level, absol_error) / batch_size)+1
+
+def controlled_simulation(params: HestonParameters,
+                          simulate: Callable[[HestonParameters, int], np.ndarray],
+                          absolute_error: float = 0.01,
+                          batch_size: int = 10000):
+    """Controlled-precision simulation using the batch method.
+    Args:
+        params: The Heston parameters to use.
+        simulate: The simulation function to use.
+        absolute_error: The absolute error to achieve. Defaults to 0.01.
+
+    Returns:
+        The paths.
+    """
+
+    data = simulate(params, 100)[:, -1]
+    batches = get_number_of_batches(data, 0.05, absolute_error)
+    for i in range(batches):
+        data = np.concatenate((data, simulate(params, batch_size)[:, -1]), axis=0)
+
+    return data
